6cbf829cb5e4842377ff3fde52b690328c47d84d
sparsh-Tyagi01 <sparshtyagi630@gmail.com>
2025-08-22 00:43:43 +0530

    update dashboard

--- frontend/src/components/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard.jsx
@@ -1,11 +1,11 @@
-import { Activity, Droplet, ShoppingBasket, Users } from "lucide-react";
+import { Activity, Delete, Droplet, ShoppingBasket, Users } from "lucide-react";
 import { motion } from "motion/react";
 
 const AdminDashboard = () => {
 
     const dontors = [
-        {name: "John", email: "john@gmail.com", category: "Organ", type: "Kidney"},
-        {name: "Tanish Goel", email: "tanish@gmail.com", category: "Blood", type: "A+"}
+        {name: "John", email: "john@gmail.com", mobile: "9234204793", category: "Organ", type: "Kidney"},
+        {name: "Harry", email: "harry@gmail.com", mobile: "7536537356", category: "Blood", type: "A+"}
     ]
 
   return (
@@ -36,7 +36,7 @@ const AdminDashboard = () => {
         <div className="flex justify-center items-center gap-4 mt-4">
           <motion.div
            whileHover={{scale:1.02}}
-           className="w-[38vw] h-[50vh] bg-gray-950 rounded-[10px] shadow-[0px_0px_5px_green] flex flex-col items-center justify-center">
+           className="w-[38vw] h-[30vh] bg-gray-950 rounded-[10px] shadow-[0px_0px_5px_green] flex flex-col items-center justify-center">
             <div className="flex items-center justify-center gap-2 text-white font-medium text-xl">
               <ShoppingBasket size={40} className="text-green-500 brightness-200" /> Orders
             </div>
@@ -44,14 +44,14 @@ const AdminDashboard = () => {
           </motion.div>
           <motion.div
            whileHover={{scale:1.02}}
-           className="w-[38vw] h-[50vh] bg-gray-950 rounded-[10px] shadow-[0px_0px_5px_yellow] flex flex-col items-center justify-center">
+           className="w-[38vw] h-[30vh] bg-gray-950 rounded-[10px] shadow-[0px_0px_5px_yellow] flex flex-col items-center justify-center">
             <div className="flex items-center justify-center gap-2 text-white font-medium text-xl">
               <Activity size={40} className="text-yellow-500 brightness-200" /> Active Staff
             </div>
             <p className="text-white text-2xl font-bold mt-2">27</p>
           </motion.div>
         </div>
-        <div className="flex flex-col items-center w-[76vw] h-[70vh] rounded-[10px] bg-gray-950 mt-4 shadow-[0px_0px_5px_pink] overflow-y-scroll scrollbar-hide">
+        <div className="flex flex-col items-center w-[76vw] h-[50vh] rounded-[10px] bg-gray-950 mt-4 shadow-[0px_0px_5px_pink] overflow-y-scroll scrollbar-hide">
             <div className="text-white font-medium text-xl mt-2">
               Request to Donate
             </div>
@@ -71,6 +71,41 @@ const AdminDashboard = () => {
                   <td className="py-2 px-3">{items.email}</td>
                   <td className="py-2 px-3 text-red-600">{items.category}</td>
                   <td className="py-2 px-3 text-green-400">{items.type}</td>
+                </tr>
+                ))}
+              </tbody>
+            </table>
+        </div>
+        <div className="flex flex-col items-center w-[76vw] h-[50vh] rounded-[10px] bg-gray-950 mt-4 shadow-[0px_0px_5px_violet] overflow-y-scroll scrollbar-hide">
+          <div className="text-white font-medium text-xl mt-2">
+              Request to get Organ/Blood
+            </div>
+            <table className="w-full text-left text-white border-collapse mt-2">
+              <thead>
+                <tr className="border-b border-gray-700">
+                  <th className="py-2 px-3">Name</th>
+                  <th className="py-2 px-3">Email</th>
+                  <th className="py-2 px-3">Mobile NO</th>
+                  <th className="py-2 px-3">Category</th>
+                  <th className="py-2 px-3">type</th>
+                  <th className="py-2 px-3">Info</th>
+                  <th className="py-2 px-3">Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dontors.map(items => (
+                    <tr key={items.email} className="border-b border-gray-800">
+                  <td className="py-2 px-3">{items.name}</td>
+                  <td className="py-2 px-3">{items.email}</td>
+                  <td className="py-2 px-3">{items.mobile}</td>
+                  <td className="py-2 px-3 text-red-600">{items.category}</td>
+                  <td className="py-2 px-3 text-green-400">{items.type}</td>
+                  <td className="py-2 px-3 text-white">
+                    <motion.button whileTap={{scale:.95}} className="cursor-pointer py-1 px-4 rounded-[5px] bg-yellow-600 font-medium">Send</motion.button>
+                  </td>
+                  <td>
+                    <motion.button whileTap={{scale:.95}} className="cursor-pointer py-1 px-4 rounded-[5px] text-red-600"><Delete/></motion.button>
+                  </td>
                 </tr>
                 ))}
               </tbody>
